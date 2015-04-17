c394ff6f6911257467997795df306687dea1f9e8
Garreth Dottin <gdottin@liveintent.com>
2015-04-16 21:49:53 -0400

    initial commit: fixed bug with 340527 port in use with grunt watch

---
+++ server/auth/twitter/passport.js
@@ -0,0 +1,36 @@
+exports.setup = function (User, config) {
+  var passport = require('passport');
+  var TwitterStrategy = require('passport-twitter').Strategy;
+
+  passport.use(new TwitterStrategy({
+    consumerKey: 'o4Vg5Q4D8iZ6RsBssEQf1MYwB',
+    consumerSecret: 'K9EB32wURCbYElXtvpKP8usqrtJ4hoPhrv7fVcK5HrCn11db3w',
+    callbackURL: 'http://localhost:9000/'
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOne({
+      'twitter.id_str': profile.id
+    }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        console.log('user has been created')
+        user = new User({
+          name: profile.displayName,
+          username: profile.username,
+          role: 'user',
+          provider: 'twitter',
+          twitter: profile._json
+        });
+        user.save(function(err) {
+          if (err) return done(err);
+          return done(err, user);
+        });
+      } else {
+        return done(err, user);
+      }
+    });
+    }
+  ));
+};
