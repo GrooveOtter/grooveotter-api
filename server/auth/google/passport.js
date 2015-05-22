96340ee8446df57415a40caf13fb3ad329281ca6
Garreth Dottin <gdottin@liveintent.com>
2015-05-22 19:17:39 -0400

    Fixed google auth

--- server/auth/google/passport.js
+++ server/auth/google/passport.js
@@ -3,8 +3,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 exports.setup = function (User, config) {
   passport.use(new GoogleStrategy({
-      clientID: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientID: local.env.GOOGLE_CONSUMER_KEY,
+      clientSecret: local.env.GOOGLE_CONSUMER_SECRET,
       callbackURL: 'http://grooveotter-api.herokuapp.com/auth/google/callback'
     },
     function(accessToken, refreshToken, profile, done) {
