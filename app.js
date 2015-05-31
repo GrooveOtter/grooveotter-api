0c008407a23656efe85309f12aaf9f9cb1592426
Tenor Biel <tenorbiel@gmail.com>
2015-05-31 07:32:52 -0500

    enable session resaving

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(session({
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     },
     saveUninitialized: false,
-    resave: false
+    resave: true
 }));
 
 app.use(passport.initialize());
