42053eae80d4ef44d0417a4edc7706b40dc3fc65
Tenor Biel <tenorbiel@gmail.com>
2015-06-14 16:08:21 -0500

    rename shared_tasks -> newsfeed

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ api.use(roles.ensureLoggedIn);
 
 api.use('/users', require('./users'));
 api.use('/tasks', require('./tasks'));
-api.use('/shared-tasks', require('./shared_tasks'));
+api.use('/newsfeed', require('./newsfeed'));
 
 api.use(function handleNotFoundError(err, req, res, next) {
     if (err instanceof bookshelf.Model.NotFoundError) {
