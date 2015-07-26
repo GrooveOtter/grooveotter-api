33ab70af43cee5810f35a95f52d751ecd7d167b7
Tenor Biel <tenorbiel@gmail.com>
2015-07-26 11:49:15 -0500

    upgrade sse system to allow all origins

--- channel.js
+++ channel.js
@@ -5,6 +5,7 @@ var Channel = require('sse-channel');
 var channel = module.exports = new Channel({
     jsonEncode: true,
     cors: {
-        origins: ['https://grooveotter-demo.herokuapp.com', 'http://localhost', 'http://localhost:8000']
+        origins: ['*'],
+        credentials: true
     }
 });
