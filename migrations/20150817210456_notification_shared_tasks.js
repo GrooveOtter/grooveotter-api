5c7d2648c29c2fae0c18cfa0aefebcc42fd16aeb
Garreth Dottin <gdottin@liveintent.com>
2015-08-21 22:29:17 -0400

    Use JavaScript dates instead of unix

--- migrations/20150817210456_notification_shared_tasks.js
+++ migrations/20150817210456_notification_shared_tasks.js
@@ -2,7 +2,7 @@ var uuid = require('uuid');
 exports.up = function(knex, Promise) {
   return knex('tasks').then(function(tasks) {
     return Promise.all(tasks.map(function(task) {
-      return knex('notifications').insert({id: uuid.v4(), task_id: task.id, text: '', user_id: task.user_id, created_at: Date.now(), updated_at: Date.now()});
+      return knex('notifications').insert({id: uuid.v4(), task_id: task.id, text: '', user_id: task.user_id, created_at: new Date(), updated_at: new Date()});
       }))
   });
 };
